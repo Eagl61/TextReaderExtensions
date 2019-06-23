ccfd2585fbb108acda480910e753307689efa3d1
Evgeny <ihavenoemail>
2019-06-23 19:40:09 +0300

    Added the sample of using an extension.

---
+++ Sample/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using TextReaderExtensions;
+
+namespace Sample
+{
+    static class Program
+    {
+        static void Main()
+        {
+            Console.Write("Enter an integer value: ");
+            Console.WriteLine($"You entered {Console.In.ReadInteger()}");
+        }
+    }
+}
